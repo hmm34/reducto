dd412c3fd8e862bbd516a3f6b5aff94d3431e186
Heather Michaud <hmm34@zips.uakron.edu>
2014-02-16 14:24:40 -0500

    Add command line interface

--- src/reducto.cpp
+++ src/reducto.cpp
@@ -6,10 +6,86 @@
 */
 
 #include <iostream>
+#include <cstdlib>
+
+std::string ex = "some" "other";
+std::string usage =  "Expected usage: \n"
+							"\treducto 1 <image.pgm> \n"
+							"\treducto 2 <image_b.pgm> \n" 
+							"\treducto 3 <header.txt> <SVD.txt> k \n"
+							"\treducto 4 <image_b.pgm.SVD> \n";
+
+const std::string ASCII_TO_BINARY = "1";
+const std::string BINARY_TO_ASCII = "2";
+const std::string COMPRESS_PGM    = "3";
+const std::string DECOMPRESS_PGM  = "4";
+
+//! @brief Verifies that there are the expected number of arguments and, if
+//! 	not, returns false
+//! and prints out the expected usage.
+//! @param argc The number of arguments
+//! @param expectedArgs The expected number of arguments
+//! @retval true  If there are three arguments
+//! @retval false If there aren't three arguments
+bool areValidArguments(int argc, int expectedArgs)
+{
+	if (argc != expectedArgs)
+	{
+		std::cout << usage;
+		return false;
+	}
+	return true;
+}
 
 int main(int argc, const char* argv[])
 {
-	std::cout << "Fun with linear algebra!!\n";
+	if (argc < 2 || argc > 5)
+	{
+		std::cout << usage;
+		return 1;	
+	}
+
+	std::string option = argv[1];
+	std::string file   = argv[2];
+	if (option == ASCII_TO_BINARY)
+	{
+		if (!areValidArguments(argc, 3))
+			return 2;
+
+		// file is the PGM image to convert to binary
+		// Do things!
+	}
+	else if (option == BINARY_TO_ASCII)
+	{
+		if (!areValidArguments(argc, 3))
+			return 3;
+
+		// file is the binary file to convert to PGM
+		// Do things!
+	}
+	else if (option == COMPRESS_PGM)
+	{
+		if (!areValidArguments(argc, 5))
+			return 4;
+
+		// file is the header.txt
+		std::string svd = argv[3];	// svd.txt
+		int k = atoi(argv[4]);		// k
+		// Do things!
+	}
+	else if (option == DECOMPRESS_PGM)
+	{
+		if (!areValidArguments(argc, 3))
+			return 5;
+
+		// file is the compressed image that needs decompressed
+		// Do things!
+	}
+	else
+	{
+		std::cout << usage;
+		return 6;
+	}
 
 	return 0;
 }
