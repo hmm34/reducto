d404194b2268037152065d48781bcf9a0fd522c7
Heather Michaud <hmm34@zips.uakron.edu>
2014-02-19 20:56:55 -0500

    Add timing for binary to ascii

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -99,35 +99,47 @@ void runTimingTests()
 
 	std::string resultFile = "timing-results.csv";
 	std::ofstream output(resultFile);
-	output << "width, height, total pixels, average time (of 10 runs) in ms\n";
+	output << "width, height, total pixels, ";
+	output << "ascii to binary average time (of 10 runs) in ms, ";
+	output << "binary to ascii average time (of 10 runs) in ms\n";
 
-	std::string test = "test.pgm";
+	std::string test1 = "test.pgm";
+	std::string test2 = "test_b.pgm";
 	int maxSize = 500;
 	for (int size = 50; size <= maxSize; size+=50)
 	{
-		long double msTotal = 0;
+		long double msTotal1 = 0; // for ascii to binary
+		long double msTotal2 = 0; // for binary to ascii
 		int numRuns = 10;
 		for (int times = 0; times < numRuns; ++times)
 		{
-			createPGM(test.c_str(), size, size);
+			createPGM(test1.c_str(), size, size);
 
-			std::clock_t start = std::clock();
-			reducto::asciiToBinary(test);
-			std::clock_t end = std::clock();
+			std::clock_t start1 = std::clock();
+			reducto::asciiToBinary(test1);
+			std::clock_t end1 = std::clock();
 
-			remove(test.c_str());
-			msTotal += 1000.0 * (end - start) / CLOCKS_PER_SEC; // time in ms
+			std::clock_t start2 = std::clock();
+			reducto::binaryToAscii(test2);
+			std::clock_t end2 = std::clock();
+
+			remove(test1.c_str());
+			remove(test2.c_str());
+			msTotal1 += 1000.0 * (end1 - start1) / CLOCKS_PER_SEC;
+			msTotal2 += 1000.0 * (end2 - start2) / CLOCKS_PER_SEC;
 		}
 		std::cerr << "Timing: " << size << " of " << maxSize << "\r";
 
-		double average = msTotal / (double)numRuns;
-		output << size << ", " << size << ", " << size * size << ", " << average << "\n";
+		double average1 = msTotal1 / (double)numRuns;
+		double average2 = msTotal2 / (double)numRuns;
+		output << size << ", " << size << ", " << size * size << ", ";
+		output << average1 << "," << average2 << "\n";
 	}
 	std::cerr << "\n";
 
 	// Clean up
-	int lastIndex = test.find_last_of(".");
-	std::string oFile = test.substr(0, lastIndex) + "_b.pgm";
+	int lastIndex = test1.find_last_of(".");
+	std::string oFile = test1.substr(0, lastIndex) + "_b.pgm";
 	remove(oFile.c_str());
 	output.close();
 }
